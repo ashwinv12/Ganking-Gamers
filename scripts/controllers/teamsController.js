d60f939e10480a9d1dd87da39a7ecf68b8c33d37
Ashwin <ashwinmelo12@gmail.com>
2014-01-23 20:12:59 -0700

    removing team works

--- scripts/controllers/teamsController.js
+++ scripts/controllers/teamsController.js
@@ -4,6 +4,8 @@ APP.controller('teamsController', function ($scope) {
 	$scope.adding = false;
 	$scope.currentTop = 0;
 	$scope.currentLeft = 0;
+	$scope.btnCurrentTop = 0;
+	$scope.btnCurrentLeft = 0;
 	$scope.currentButton;
 
 	$scope.availableTeams = [
@@ -13,6 +15,7 @@ APP.controller('teamsController', function ($scope) {
 		{name: 'Syracuse', top: '', left: ''},
 		{name: 'Louisville', top: '', left: ''},
 		{name: 'Iowa', top: '', left: ''},
+		{name: 'George Mason', top: '', left: ''},
 	]
 
 	$scope.selectedTeams = [];
@@ -27,6 +30,17 @@ APP.controller('teamsController', function ($scope) {
 			$scope.adding = false;
 			$scope.selectingMode = false;
 		}
+	}
+
+	$scope.unselectTeam = function(team) {
+		$scope.selectedTeams.splice($scope.selectedTeams.indexOf(team), 1);
+		$scope.btnCurrentTop = team.top;
+		$scope.btnCurrentLeft = team.left;
+		var button = {left: $scope.btnCurrentLeft, top: $scope.btnCurrentTop};
+		$scope.buttons.push(button);
+		$scope.availableTeams.push(team);
+		// $scope.adding = false;
+		// $scope.selectingMode = false;
 	}
 
 	$scope.beginSelecting = function(button) {
@@ -49,10 +63,10 @@ APP.controller('teamsController', function ($scope) {
 
 	$scope.firstRoundGames = [
 
-		{x1: 0, x2: 120, y1: 170, y2: 130, x3: 0, x4: 120, y3: 270, y4: 230},
-		{x1: 0, x2: 120, y1: 370, y2: 330, x3: 0, x4: 120, y3: 470, y4: 430},
-		{x1: 0, x2: 120, y1: 570, y2: 530, x3: 0, x4: 120, y3: 670, y4: 630},
-		{x1: 0, x2: 120, y1: 770, y2: 730, x3: 0, x4: 120, y3: 870, y4: 830}
+		{x1: 0, x2: 160, y1: 170, y2: 130, x3: 0, x4: 160, y3: 270, y4: 230},
+		{x1: 0, x2: 160, y1: 370, y2: 330, x3: 0, x4: 160, y3: 470, y4: 430},
+		{x1: 0, x2: 160, y1: 570, y2: 530, x3: 0, x4: 160, y3: 670, y4: 630},
+		{x1: 0, x2: 160, y1: 770, y2: 730, x3: 0, x4: 160, y3: 870, y4: 830}
 		
 	]
 
@@ -62,14 +76,14 @@ APP.controller('teamsController', function ($scope) {
 	]
 	
 	$scope.buttons = [
-		{left:'595px', top:'32px'},{left:'595px', top:'72px'},
-		{left:'595px', top:'132px'},{left:'595px', top:'172px'},
-		{left:'595px', top:'232px'},{left:'595px', top:'272px'},
-		{left:'595px', top:'332px'},{left:'595px', top:'372px'},
-		{left:'595px', top:'432px'},{left:'595px', top:'472px'},
-		{left:'595px', top:'532px'},{left:'595px', top:'572px'},
-		{left:'595px', top:'632px'},{left:'595px', top:'672px'},
-		{left:'595px', top:'732px'},{left:'595px', top:'772px'},
+		{left:'545px', top:'32px'},{left:'545px', top:'72px'},
+		{left:'545px', top:'132px'},{left:'545px', top:'172px'},
+		{left:'545px', top:'232px'},{left:'545px', top:'272px'},
+		{left:'545px', top:'332px'},{left:'545px', top:'372px'},
+		{left:'545px', top:'432px'},{left:'545px', top:'472px'},
+		{left:'545px', top:'532px'},{left:'545px', top:'572px'},
+		{left:'545px', top:'632px'},{left:'545px', top:'672px'},
+		{left:'545px', top:'732px'},{left:'545px', top:'772px'},
 	]
 
 
