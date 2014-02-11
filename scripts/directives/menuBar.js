2e6aa54ea9743d4a06d09690a913eb85cd1643ca
Ashwin <ashwinmelo12@gmail.com>
2014-02-11 12:22:27 -0700

    menu items obtain active class correctly

--- scripts/directives/menuBar.js
+++ scripts/directives/menuBar.js
@@ -4,7 +4,18 @@ APP.directive('menuBar', function () {
 		templateUrl: 'menu.html',
 		replace: true,
 		controller: function($scope) {
+			$scope.currentPageIndex = 0;
 
+			$scope.menuItems = [
+				{name: 'Home', icon: 'home'},
+				{name: 'Walkthroughs'},
+				{name: 'Gameplays'},
+				{name: 'About'},
+			]
+
+			$scope.setCurrentPage = function(index) {
+				$scope.currentPageIndex = index;
+			}
 		},
 	}
 })
